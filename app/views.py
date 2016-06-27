7afbe5f3662725bb5d770ef2af5b82012e456916
rachhtordoff <rachael.tordoff@hotmail.co.uk>
2016-06-27 15:40:28 +0100

    changes to homepage, views and also added calc page

--- app/views.py
+++ app/views.py
@@ -1,6 +1,12 @@
 from app import app
-from flask import render_template
+from flask import render_template, request
 @app.route('/')
-@app.route('/Homepage')
-def index():
+@app.route('/homepage')
+def homepage():
     	return render_template('Homepage.html')
+		
+@app.route('/calculator-page', methods=['GET'])
+def calculator():
+
+	return render_template('calculator.html')
+	
