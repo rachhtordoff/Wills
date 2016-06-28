878f1b696bec206e95bfb3354911b10081956db4
rachhtordoff <rachael.tordoff@hotmail.co.uk>
2016-06-28 14:44:34 +0100

    pages added and calc added- flask used

--- app/views.py
+++ app/views.py
@@ -1,5 +1,13 @@
 from app import app
-from flask import render_template, request
+from flask import render_template, request, session,  url_for, redirect
+
+def sumSessionCounter():
+  try:
+	session['name'] = request.form['username']
+  except KeyError:
+    session['name'] = 'error'
+
+
 @app.route('/')
 @app.route('/homepage')
 def homepage():
@@ -10,3 +18,19 @@ def calculator():
 
 	return render_template('calculator.html')
 	
+	
+@app.route('/signup', methods=['POST'])
+def signup():
+    sumSessionCounter()
+
+    return redirect(url_for('message'))
+
+@app.route('/message')
+def message():
+    if not 'name' in session:
+        return abort(403)
+    return render_template('calculator.html', username=session['name'])
+										   
+
+if __name__ == '__main__':
+    app.run()
